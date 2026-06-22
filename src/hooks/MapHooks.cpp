186f853c6b66a64d265117d2ee55b1584e9c2e25
megax <mtellom@correo.uss.cl>
2026-06-22 19:17:53 -0400

    Map node sync fix

--- src/hooks/MapHooks.cpp
+++ src/hooks/MapHooks.cpp
@@ -12,54 +12,59 @@ void *g_MapScreen = nullptr;
 bool g_isHandlingNetworkMapNodeSync = false;
 uint32_t g_pendingMapNodeSyncIndex = 0xFFFFFFFF;
 
-HOOK_DEFINE(MapScreen_EnterNode, void, void *, void *)
+HOOK_DEFINE(MapNode_Click, void, void *)
 
-static void __fastcall Hook_MapScreen_EnterNode(void *self, void *node) {
+static void __fastcall Hook_MapNode_Click(void *self) {
   if (!NetworkManager::Get().GetCurrentLobby().IsValid()) {
-    if (g_origMapScreen_EnterNode) g_origMapScreen_EnterNode(self, node);
+    if (g_origMapNode_Click) g_origMapNode_Click(self);
     return;
   }
 
-  g_pendingMapNodeSyncIndex = 0xFFFFFFFF; // Clear any pending sync on entering a node
+  uint32_t nodeIndex = 0xFFFFFFFF;
 
-  if (!g_isHandlingNetworkMapNodeSync) {
-    if (NetworkManager::Get().IsInputBlocked(SteamUser()->GetSteamID().ConvertToUint64())) {
-      Overlay::Log("[MAP] Ignoring local EnterNode (Input Blocked)");
-      return; 
+  if (self && GameUtils::IsComponentValid(g_MapScreen)) {
+    const void* targetMapNode = nullptr;
+    
+    if ((uintptr_t)self > 0x10000) {
+        targetMapNode = *(void**)((char*)self + 8);
     }
 
-    uint32_t nodeIndex = 0xFFFFFFFF;
-    if (self && node) {
-      // Find the index of the node in the MapScreen's podvector
-      const auto *mapScreen = static_cast<const glaiel::MapScreen*>(self);
-      const uint32_t vectorSize = mapScreen->nodes.size_;
-      void **nodes = mapScreen->nodes.data_;
-      if (nodes) {
-        for (uint32_t i = 0; i < vectorSize; i++) {
-          if (nodes[i] == node) {
-            nodeIndex = i;
-            break;
-          }
+    const auto *mapScreen = static_cast<const glaiel::MapScreen*>(g_MapScreen);
+    const uint32_t vectorSize = mapScreen->nodes.size_;
+    if (void **nodes = mapScreen->nodes.data_) {
+      for (uint32_t i = 0; i < vectorSize; i++) {
+        if (nodes[i] == targetMapNode) {
+          nodeIndex = i;
+          break;
         }
       }
     }
-
-    if (nodeIndex != 0xFFFFFFFF) {
-      MapNodeSyncPacket pkt = {};
-      pkt.nodeIndex = nodeIndex;
-      NetworkManager::Get().BroadcastPacket(PacketType::MapNodeSync, &pkt, sizeof(pkt), true);
-      Overlay::Log("[MAP] Broadcasted MapNodeSync for index %u", nodeIndex);
-    } else {
-      Overlay::Log("[MAP] [ERR] Could not find MapNode in MapScreen array!");
-    }
   }
 
+  if (nodeIndex != 0xFFFFFFFF) {
+    MapNodeSyncPacket pkt = {};
+    pkt.nodeIndex = nodeIndex;
+    NetworkManager::Get().BroadcastPacket(PacketType::MapNodeSync, &pkt, sizeof(pkt), true);
+    Overlay::Log("[MAP] Broadcasted MapNodeSync for index %u", nodeIndex);
+  } else {
+    Overlay::Log("[MAP] [ERR] Could not find clicked MapNode in MapScreen array!");
+  }
+
+  if (g_origMapNode_Click) {
+    g_origMapNode_Click(self);
+  }
+}
+
+HOOK_DEFINE(MapScreen_EnterNode, void, void *, void *)
+
+static void __fastcall Hook_MapScreen_EnterNode(void *self, void *node) {
+  g_pendingMapNodeSyncIndex = 0xFFFFFFFF; // Clear any pending sync on entering a node
   if (g_origMapScreen_EnterNode) {
     g_origMapScreen_EnterNode(self, node);
   }
 }
 
-void TriggerMapNodeSync(uint32_t nodeIndex) {
+void TriggerMapNodeSync(const uint32_t nodeIndex) {
   if (!GameUtils::IsComponentValid(g_MapScreen)) {
     g_pendingMapNodeSyncIndex = nodeIndex;
     Overlay::Log("[MAP] MapScreen is null or inactive, queued MapNodeSync for index %u", nodeIndex);
@@ -81,17 +86,23 @@ void TriggerMapNodeSync(uint32_t nodeIndex) {
     return;
   }
 
-  g_isHandlingNetworkMapNodeSync = true;
-  if (g_origMapScreen_EnterNode) {
-    Overlay::Log("[MAP] Triggering network-synced EnterNode (index %u)", nodeIndex);
-    g_origMapScreen_EnterNode(g_MapScreen, matchedNode);
+  if (g_origMapNode_Click) {
+    Overlay::Log("[MAP] Triggering network-synced Click (index %u)", nodeIndex);
+    
+    // The hooked function is a lambda operator() that expects a closure object in RCX.
+    // It only ever reads `[RCX + 8]` to get the captured MapNode pointer.
+    // We can simulate this by constructing a dummy closure array!
+    void* dummyClosure[2] = { nullptr, matchedNode };
+    g_origMapNode_Click(dummyClosure);
   }
-  g_isHandlingNetworkMapNodeSync = false;
 }
 
 void MapHooks_Init(MewjectorAPI *mj, const uintptr_t gameBase) {
-  // Should use a different one
   HOOK_INSTALL(mj, gameBase, MapScreen_EnterNode,
     "48 8b c4 48 89 58 08 55 56 57 41 54 41 55 41 56 41 57 48 8d a8 e8 fe ff ff 48 81 ec e0 01 00 00 0f 29 70 b8 0f 29 78 a8 48 8b fa 48 8b f1 33 c0",
+    0);
+
+  HOOK_INSTALL(mj, gameBase, MapNode_Click,
+    "48 89 5c 24 20 55 56 57 41 56 41 57 48 81 ec c0 00 00 00 48 8b 71 08 83 be 38 01 00 00 04 0f 85",
     0);
 }
